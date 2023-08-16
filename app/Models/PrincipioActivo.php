bdaee41e1c818f18621e7796ce0e5653c858152e
Augusto Britez <agussb52@gmail.com>
2023-08-16 14:21:25 -0300

    principio activo controller

--- app/Models/PrincipioActivo.php
+++ app/Models/PrincipioActivo.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class PrincipioActivo extends Model
 {
     use HasFactory;
+    
+    static function loadById($id) {
+        return PrincipioActivo::query()->find($id);
+    }
 }
