0cfc4d5bdd301c725214ee1dd62b458bca03bdcf
Augusto Britez <agussb52@gmail.com>
2023-08-19 20:34:14 -0300

    mend
    api definition

--- resources/frontend/src/app/app.component.ts
+++ resources/frontend/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'farmacia-frontend';
+  usuarios = [];
+  constructor(private authService: AuthService) {
+
+  }
+
+  ngOnInit(): void {
+    this.authService.getUser().subscribe( 
+      {
+        next: (data: any) => {
+          this.usuarios = data;
+        }
+      }
+    );
+  }
 }
